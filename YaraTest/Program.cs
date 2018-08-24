8f0a47b5ba99b65e83cf9967894e184de1d618fe
stellarbears <stellarbears@gmail.com>
2018-08-24 11:20:45 +0300

    Typo fix

--- YaraTest/Program.cs
+++ YaraTest/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Collections.Generic;
-
 using YaraSharp;
 
 namespace YaraTest
@@ -16,7 +15,7 @@ namespace YaraTest
 
             //  All API calls happens here
             YaraSharp.CYaraSharp YSInstance = new CYaraSharp();
-            
+
             //  Declare external variables (could be null)
             Dictionary<string, object> Externals = new Dictionary<string, object>()
             {
@@ -27,16 +26,16 @@ namespace YaraTest
 
             //  Errors occured during rule compilation: ignored_file : List<reasons>
             Dictionary<string, List<string>> Errors = new Dictionary<string, List<string>>();
-            
+
             //  Context is where yara is initialized
             //  From yr_initialize() to yr_finalize()
             using (YaraSharp.CContext YSContext = new YaraSharp.CContext())
             {
                 //	Compiling rules
-                using (YaraSharp.CRules YSRules = YSInstance.CompileFromFiles(Directory.GetFiles(@"C:\Users\root\source\repos\aptscan-sources\APTScan\Build\Debug\AnyCPU\signatures\yara", "*.yar", SearchOption.AllDirectories).ToList(), Externals, out Errors))
+                using (YaraSharp.CRules YSRules = YSInstance.CompileFromFiles(Directory.GetFiles(@"<path to signatures>", "*.yar", SearchOption.AllDirectories).ToList(), Externals, out Errors))
                 {
                     //  Some file to test yara rules
-                    string Filename = @"\\?\D:\Test\DWS.pdf";
+                    string Filename = @"\\?\<path to file>";
 
                     //  Get matches
                     List<YaraSharp.CMatches> Matches = YSInstance.ScanFile(Filename, YSRules,
