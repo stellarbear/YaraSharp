6e59582dfd03c9ee5c2bf3c77c77dcc0de4d07fa
stellarbears <stellarbears@gmail.com>
2018-08-27 10:19:55 +0300

    Hot fix

--- YaraSharp/AssemblyInfo.cpp
+++ YaraSharp/AssemblyInfo.cpp
@@ -31,8 +31,8 @@ using namespace System::Security::Permissions;
 // You can specify all the value or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersion("1.1.3")];
-[assembly:AssemblyFileVersion("1.1.3")];
+[assembly:AssemblyVersion("1.2.2")];
+[assembly:AssemblyFileVersion("1.2.2")];
 
 [assembly:ComVisible(false)];
 
